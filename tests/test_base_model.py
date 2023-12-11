0bbaf5a683d978c682f2a592c55f338f81fa0403
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 00:11:12 +0200

    Add unit tests for BaseModel class

---
+++ tests/test_base_model.py
@@ -0,0 +1,36 @@
+import models
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+import uuid
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_str(self):
+        model = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(model.id, model.__dict__)
+        self.assertEqual(str(model), expected_str)
+
+    def test_save(self):
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        self.assertNotEqual(old_updated_at, model.updated_at)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['id'], model.id)
+        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+
+if __name__ == '__main__':
+    unittest.main()
+
