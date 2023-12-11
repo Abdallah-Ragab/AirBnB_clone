4aaf974c18e6a783d293013320acb30dd00d3226
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 00:23:32 +0200

    PEP8

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from datetime import datetime
 import uuid
 
+
 class TestBaseModel(unittest.TestCase):
     def test_init(self):
         model = BaseModel()
@@ -29,9 +30,16 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(model_dict, dict)
         self.assertEqual(model_dict['__class__'], 'BaseModel')
         self.assertEqual(model_dict['id'], model.id)
-        self.assertEqual(model_dict['created_at'], model.created_at.isoformat())
-        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+        self.assertEqual(
+            model_dict['created_at'],
+            model.created_at.isoformat()
+            )
+        self.assertEqual(
+            model_dict['updated_at'],
+            model.updated_at.isoformat()
+            )
+
 
 if __name__ == '__main__':
-    unittest.main()
 
+    unittest.main()
