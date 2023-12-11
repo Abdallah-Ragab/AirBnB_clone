dc1e974503718c80702e8faa273d6f2e5960a873
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 00:11:01 +0200

    Update BaseModel with datetime module import and add __init__ method

--- models/base_model.py
+++ models/base_model.py
@@ -4,8 +4,14 @@ import uuid
 
 class BaseModel:
     id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    created_at = datetime.datetime.now()
+    updated_at = datetime.datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for kw in kwargs:
+                if not kw == "__class__":
+                    setattr(self, kw, kwargs[kw])
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -15,7 +21,7 @@ class BaseModel:
         )
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
