7fc7e3d817323aa802a183867b9fb09ae429fa8b
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-11 23:15:23 +0200

    Add BaseModel class with basic attributes and methods

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+import datetime
+import uuid
+
+
+class BaseModel:
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
