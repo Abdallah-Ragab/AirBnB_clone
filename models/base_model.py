67575603851d37c42e27682ea373afa0ad099086
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 00:18:29 +0200

    Add id attribute to obj_dict in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -30,4 +30,5 @@ class BaseModel:
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["id"] = self.id
         return obj_dict
