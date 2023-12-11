de131952760e6b33f40b5bf64c273de0b9713433
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 01:27:07 +0200

    Add storage functionality to BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+from . import storage
 
 
 class BaseModel:
@@ -15,6 +16,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(
@@ -25,6 +27,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
