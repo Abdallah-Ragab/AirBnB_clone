0794f6ed2be05a590843f7d6abf41031da2e00ca
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 01:03:12 +0200

    Remove unnecessary code in BaseModel constructor

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            self.storage.new(self)
 
 
     def __str__(self):
