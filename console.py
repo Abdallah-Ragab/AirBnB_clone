467152d99d4af03148e379aa98640adf7820c799
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 18:28:24 +0200

    Commented out unused imports

--- console.py
+++ console.py
@@ -4,11 +4,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
+# from models.state import State
+# from models.city import City
+# from models.place import Place
+# from models.amenity import Amenity
+# from models.review import Review
 
 
 def parse(arg):
