3b93db395ca19d783b5009362c6df3f97e4521cf
Abdallah Sameh Ragab <abdallahsameh1140@gmail.com>
2023-12-12 18:14:20 +0200

    Add console.py with HBNBCommand class

---
+++ console.py
@@ -0,0 +1,34 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Amenity",
+        "Place",
+        "Review"
+    }
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Empty line"""
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
